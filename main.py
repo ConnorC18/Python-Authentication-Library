10a56f814775d63c691d8fd670e7731820b9efa1
ConnorC18 <connor.coleman2002@gmail.com>
2019-03-23 22:25:17 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from loginSystem import *
 
-print(passwordReset())          # Will return True or False
 print(signUp())                 # Will return True or the error message thrown by the function
 print(login())                  # Will return True or "incorrectinfo" or "noaccount" depending on the error.
 print(sessionInfo["username"])  # Will return the current logged in users, username
